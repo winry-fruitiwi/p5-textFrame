37a62a7ba0e237dca8232e582e043df55256a35f
Winry <winry.tian@gmail.com>
2022-02-06 02:28:28 -0800

    modify coding plan

--- sketch.js
+++ sketch.js
@@ -8,7 +8,11 @@
  *     create the diagonal stroke, including the extra weight
  *     create the horizontal stroke, with and without the extra weight
  *     make the transparent rectangle
- *     reflect the corners
+ *     method 1: save the corners and translate/rotate them
+ *         save the corners using a PGraphics object
+ *         translate the corners and rotate them
+ *     optional method 2: use reflect()
+ *         best shot: try to reflect every single vector after translating!
  */
 let font
 
