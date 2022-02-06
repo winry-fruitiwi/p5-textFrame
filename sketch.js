4cfb4a8393f8c468b8026661af31740e0438c400
Winry <winry.tian@gmail.com>
2022-02-06 14:42:16 -0800

    add systematic corner planning

--- sketch.js
+++ sketch.js
@@ -3,10 +3,7 @@
  * @date 2022.02.5
  *
  * Coding plan:
- *     figure out exact coordinates
- *     create the vertical stroke, including the extra weight
- *     create the diagonal stroke, including the extra weight
- *     create the horizontal stroke, with and without the extra weight
+ *     create first corner of textFrame
  *     make the transparent rectangle
  *     method 1: save the corners and translate/rotate them
  *         save the corners using a PGraphics object
@@ -23,10 +20,48 @@ function preload() {
 function setup() {
     createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
+
+    noFill()
+    stroke(187, 20, 98)
+    strokeWeight(10)
+    strokeCap(SQUARE)
+    strokeJoin(MITER)
+}
+
+// displays a corner of the textFrame.
+function displayCornerOfFrame() {
+    // before I use a PGraphics object, I'll draw my frame corner on a blank
+    // background.
+    noFill()
+    stroke(187, 20, 98)
+    strokeWeight(10)
+    strokeCap(SQUARE)
+    strokeJoin(MITER)
+
+    beginShape()
+    let firstStrokeStart = new p5.Vector(20, 90)
+    vertex(firstStrokeStart.x, firstStrokeStart.y)
+
+    let diagonalStart = new p5.Vector(20, 50)
+    vertex(diagonalStart.x, diagonalStart.y)
+
+    let diagonalEnd = new p5.Vector(50, 30)
+    vertex(diagonalEnd.x, diagonalEnd.y)
+
+    let thickHorizontalEnd = new p5.Vector(width/4, 30)
+    vertex(thickHorizontalEnd.x, thickHorizontalEnd.y)
+
+    strokeWeight(7)
+    let horizontalEnd = new p5.Vector(width/2, 30)
+    vertex(horizontalEnd.x, horizontalEnd.y)
+
+    endShape()
 }
 
 function draw() {
     background(234, 34, 24)
+
+    displayCornerOfFrame()
 }
 
 // prevent the context menu from showing up :3 nya~
