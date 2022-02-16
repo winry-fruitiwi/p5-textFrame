b202f8297a38e713e3b6ee096d855328d3a48a23
Winry <winry.tian@gmail.com>
2022-02-16 10:03:54 -0800

    add PGraphics, scale text frame. remove extra comments

--- sketch.js
+++ sketch.js
@@ -12,6 +12,8 @@
  *         best shot: try to reflect every single vector after translating!
  */
 
+let pg, textFrameQuadrant
+
 function preload() {
     // font = loadFont('data/meiryo.ttf')
 }
@@ -19,13 +21,17 @@ function preload() {
 function setup() {
     createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
+
+    // the PGraphics object. TODO don't forget the pg.'s!
+    pg = createGraphics(width/2, height/2)
+
+    pg.colorMode(HSB, 360, 100, 100, 100)
+
+    textFrameQuadrant = displayFrameQuadrant()
 }
 
 // displays a quadrant of the textFrame.
 function displayFrameQuadrant() {
-    // before I use a PGraphics object, I'll draw my frame corner on a blank
-    // background.
-
     // the starting position of my quadrant. I translate to this position later.
     // The reason why I chose (20, 20) is because I want to have a margin at the
     // top.
@@ -56,94 +62,90 @@ function displayFrameQuadrant() {
     // the gap closer for the diagonal line
     const gapCloser = 1
 
-    noFill()
-    stroke(187, 20, 98)
-    strokeWeight(7)
-    strokeCap(SQUARE)
-    strokeJoin(MITER)
+    pg.noFill()
+    pg.stroke(187, 20, 98)
+    pg.strokeWeight(7)
+    pg.strokeCap(SQUARE)
+    pg.strokeJoin(MITER)
 
     // we need to translate to our starting position! We treat that as 0, 0.
-    translate(startingPos)
+    pg.translate(startingPos)
 
     /**
      *
      */
-    beginShape()
+    pg.beginShape()
 
     // we start at the bottom of the quadrant and work our way up. This is
     // the bottommost point.
-    vertex(0, quadrantHeight)
+    pg.vertex(0, quadrantHeight)
 
     // draw the start of the bevel that connects the bottom and top lines
-    vertex(0, r)
+    pg.vertex(0, r)
 
     // draw the end of the last bevel.
-    vertex(r, 0)
+    pg.vertex(r, 0)
 
     // draw the end of the long horizontal line.
-    vertex(quadrantWidth, 0)
+    pg.vertex(quadrantWidth, 0)
 
-    endShape()
+    pg.endShape()
 
-    stroke(187, 20, 98)
-    strokeWeight(7)
+    pg.stroke(187, 20, 98)
+    pg.strokeWeight(7)
 
     /**
      * this shape makes adjustments to the original text frame quadrant such
      * that there are certain parts that are larger.
      */
-    // // we need a separate beginShape()-endShape() loop for this. If we
-    // // don't, we'd have to go back to the start of the bevel line.
-    // beginShape()
-    //
-    // // first we need to draw a shifted bevel. The coordinates are going to
-    // // be the same except they are both shifted by cornerShift or cs.
-    //
-    // // draw the start of the shifted bevel. We have to decrease the x and y
-    // // by a bit or else we'll have a triangle of the canvas in our shifted bevel
-    // vertex(
-    //     cornerShift - gapCloser,
-    //     r + cornerShift + gapCloser
-    // )
-    //
-    // // draw the end of the shifted bevel. Do the opposite operation as last time
-    // vertex(r + cornerShift + gapCloser, cornerShift - gapCloser)
-    //
-    // // first, we need to switch the strokeCap to ROUND
-    // strokeCap(ROUND)
-    //
-    // // without this extra line, the extra few pixels on the bottom of the
-    // // textFrame won't show up. But there's also a spike now!
-    // // vertex(r + cornerShift + gapCloser, cornerShift - gapCloser + 1)
-    //
-    // // now we can draw a vertex until it hits the thickLineWidth variable
-    // // vertex(thickLineWidth, thickBorderShift)
-    // // maybe it's supposed to use cornerShift
-    // vertex(thickLineWidth, cornerShift)
-    //
-    // endShape()
+    
 
     // it's actually best to make a bunch of lines
     // draw the shifted bevel
-    line(
+    pg.line(
         cornerShift - gapCloser, r + cornerShift + gapCloser,
         r + cornerShift + gapCloser, cornerShift - gapCloser
     )
 
     // set the strokeCap to ROUND for the shifted horizontal line
-    strokeCap(ROUND)
+    pg.strokeCap(ROUND)
 
     // draw the shifted horizontal line
-    line(
-        r + cornerShift + gapCloser, cornerShift - gapCloser + 2,
-        thickLineWidth, cornerShift - gapCloser + 2
+    pg.line(
+        r + cornerShift + gapCloser, thickBorderShift - gapCloser,
+        thickLineWidth, thickBorderShift - gapCloser
     )
+    // return our PGraphics as a PImage
+    return pg
 }
 
 function draw() {
     background(234, 34, 24)
 
-    displayFrameQuadrant()
+    let gapBetweenFrameHalves = -200
+
+    // attempts to display the text frame quadrant
+    // image(textFrameQuadrant, 0, 0, width, height)
+    // image(textFrameQuadrant, 0, 0, -width, height)
+    // image(textFrameQuadrant, 0, 0, width, -height)
+    // image(textFrameQuadrant, 0, 0, -width, -height)
+
+    // top left quadrant. Does not need a rotation
+    push()
+    image(textFrameQuadrant, 0, 0)
+
+    // top right
+    scale(-1, 1)
+    image(textFrameQuadrant, -width, 0)
+
+    // bottom left
+    scale(-1, -1)
+    image(textFrameQuadrant, 0, -gapBetweenFrameHalves)
+
+    // bottom right
+    scale(-1, 1)
+    image(textFrameQuadrant, -width, -gapBetweenFrameHalves)
+    pop()
 }
 
 // prevent the context menu from showing up :3 nya~
